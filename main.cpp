f8baaf5ede7ec81370be8c2d1b964bb573ae6e01
kvitadmin <kutukov@list.ru>
2021-08-19 10:15:31 +0300

    next commit

--- main.cpp
+++ main.cpp
@@ -8,6 +8,12 @@ int main() {
     std::srand(std::time(nullptr));
     const int BankSize = 30;
     const char* FileName = "../bank.bin";
+    std::string Operation;
+
+    do{
+        std::cout << "Enter operation ('+' or '-'): ";
+        std::cin >> Operation;
+    } while (Operation != "+" || Operation != "-");
 
     AddBanknotes(FileName, BankSize);
     GetBanknotes(FileName,1200);
